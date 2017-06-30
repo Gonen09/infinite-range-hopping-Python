e47f42183d52d15c3e5b699f79642da2db0fcdb3
gonen09 <gonen.rt@gmail.com>
2017-06-30 01:14:39 -0400

    Coregido espacios en lineas, error PEP

--- Problema1.py
+++ Problema1.py
@@ -3,38 +3,42 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 
-B = 15  #Temperatura inversa
+B = 15  # Temperatura inversa
 U = 0.5
-LAM =  3
+LAM = 3
 N = 10
-K = 0.3 # Entero positivo
+K = 0.3  # Entero positivo
 
-def f1(b , u , lam, N):
+
+def f1(b, u, lam, N):
     sumatoria = 0
     for n in range(1, N):
-        sumatoria+=n*math.exp(b*((u+lam-1)*n-lam*math.pow(n, 2)))
+        sumatoria += n*math.exp(b*((u+lam-1)*n-lam*math.pow(n, 2)))
     return sumatoria
 
-def f2(b , u , lam, N):
+
+def f2(b, u, lam, N):
     sumatoria = 0.0
     for n in range(N):
         resul = math.exp(b*((u+lam-1)*n-lam*math.pow(n, 2)))
-        sumatoria+= resul
+        sumatoria += resul
     return sumatoria
 
-def grafica(x, y, foto = False):
-    if (len(x)!=0 or len(y)!=0):
-        plt.plot(x, y, 'bo')
-        limInfx=min(x)-1
-        limSupx=max(x)+1
-        limInfy=min(y)-1
-        limSupy=max(y)+1
-        plt.axis([limInfx, limSupx, limInfy, limSupy]) #Longitud ejes x,y
 
-        #define plot size in inches (width, height) & resolution(DPI)
-        #plt.figure(figsize=(20, 10), dpi=100)
-        #plt.figure(figsize=(2,2))
-        if foto ==True:
+def grafica(x, y, foto=False):
+    if len(x) != 0 or len(y) != 0:
+        plt.plot(x, y, 'bo')
+        limInfx = min(x)-1
+        limSupx = max(x)+1
+        limInfy = min(y)-1
+        limSupy = max(y)+1
+        plt.axis([limInfx, limSupx, limInfy, limSupy])  # Longitud ejes x,y
+
+        # define plot size in inches (width, height) & resolution(DPI)
+        # plt.figure(figsize=(20, 10), dpi=100)
+        # plt.figure(figsize=(2,2))
+        
+        if foto == True:
             fig = plt.gcf()
             fig.set_size_inches(18.5, 10.5)
             fig.savefig('test2png.png', dpi=100)
@@ -43,10 +47,10 @@ def grafica(x, y, foto = False):
         print("Vector vacio")
 
 
-class Grafica():
+class Grafica:
     def __init__(self):
         self.x = []
-        self.listaY=[]
+        self.listaY = []
 
         self.limInfx = 0
         self.limSupx = 0
@@ -66,28 +70,28 @@ class Grafica():
             self.limInfy = min(y) - 1
             self.limSupy = max(y) + 1
         else:
-            if min(y) -1 < self.limInfy:
+            if min(y) - 1 < self.limInfy:
                 self.limInfy = min(y) -1
 
-            if max(y) +1 > self.limSupx:
+            if max(y) + 1 > self.limSupx:
                 self.limInfy = max(y) +1
 
     def ajusteEjes(self):
         pass
 
-
     def mostrarGrafico(self):
         for y in self.listaY:
-            self.ax.plot(self.x, y[0], y[1]+y[2],linewidth = 2, label = 'B: '+str(y[3]))
+            self.ax.plot(self.x, y[0], y[1]+y[2], linewidth=2, label='B: '+str(y[3]))
         legend = self.ax.legend(shadow=True)
         plt.show()
+
 
 class FuncionRo(object):
     def __init__(self):
         self.densidad_punto = 200.0
         self.rango_eje_x = [-10, 15]
-        self.u = []  #ejex
-        self.ro = [] #ejey
+        self.u = []  # ejex
+        self.ro = []  # ejey
         self.b = B # inversa de la temperatura
         self.n = N
         self.lam = LAM
@@ -114,30 +118,31 @@ class FuncionRo(object):
         return x, y
 
 
-class AnimacionDensidad():
+class AnimacionDensidad:
     def __init__(self):
         self.funcion = FuncionRo()
-        #self.figura = plt.figure()
+        # self.figura = plt.figure()
         self.fig = plt.figure()
         self.ax = plt.axes(xlim=(-10, 15), ylim=(-1, 4))
         self.line, = self.ax.plot([], [],'k.', lw=2)
         self.densidad = 1.0
+
     def init(self):
         self.line.set_data([], [])
         return self.line,
 
     def actualizacion(self, i):
         if self.funcion.densidad_punto<1000:
-            #print self.funcion.densidad_punto
-            self.funcion.densidad_punto+=self.densidad
+            # print self.funcion.densidad_punto
+            self.funcion.densidad_punto += self.densidad
             self.funcion.funcionRo()
         self.line.set_data(self.funcion.u, self.funcion.ro)
         return self.line,
 
     def actualizacion1(self, i):
-        if self.funcion.b<50:
+        if self.funcion.b < 50:
             self.ax.legend(labels=str(self.funcion.b))
-            self.funcion.b+=0.1
+            self.funcion.b += 0.1
             self.funcion.funcionRo()
         self.line.set_data(self.funcion.u, self.funcion.ro)
         return self.line,
@@ -154,7 +159,7 @@ def arreglos_B_XY(B, u, lam, N):
     for i in range(int(densidad_puntos)):
         u += (rango_de_U[1]-rango_de_U[0])/densidad_puntos
         try:
-            ro =  f1(B, u, lam, N)/f2(B, u, lam, N)
+            ro = f1(B, u, lam, N)/f2(B, u, lam, N)
             x.append(u)
             y.append(ro)
         except ZeroDivisionError:
@@ -180,24 +185,21 @@ funcion.b = 20
 funcion.funcionRo()
 grafico.agregarFuncionY(funcion.ro, 'b', '.', 15)
 """
-#grafico.mostrarGrafico()
+# grafico.mostrarGrafico()
 
 animacionB = AnimacionDensidad()
 animacionB.funcion.densidad_punto = 150.0
 animacionB.funcion.b = 0.0
 anim = animation.FuncAnimation(animacionB.fig, animacionB.actualizacion1,init_func=animacionB.init, frames=60, interval=20, blit=False)
 
-#animacionD = AnimacionDensidad()
-#animacionD.funcion.densidad_punto = 2
-#anim = animation.FuncAnimation(animacionD.fig, animacionD.actualizacion,init_func=animacionD.init, frames=60, interval=20, blit=False)
+# animacionD = AnimacionDensidad()
+# animacionD.funcion.densidad_punto = 2
+# anim = animation.FuncAnimation(animacionD.fig, animacionD.actualizacion,init_func=animacionD.init,frames=60, interval=20, blit=False)
 
+# line_ani = animation.FuncAnimation(animacionD.fig, animacionD.actualizacion, 25, interval=200, blit=True, repeat=True)
 
-
-#line_ani = animation.FuncAnimation(animacionD.fig, animacionD.actualizacion, 25, interval=200, blit=True, repeat=True)
-
-
-#anim.save('basic_animation.mp4', fps=30)
-#anim.save('im.mp4', writer="ffmpeg")
+# anim.save('basic_animation.mp4', fps=30)
+# anim.save('im.mp4', writer="ffmpeg")
 plt.show()
 
 
